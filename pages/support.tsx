4db49ccb1fa2e37181f967b38068615e2a0734d7
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-13 23:22:30 +0100

    Update support.tsx

--- pages/support.tsx
+++ pages/support.tsx
@@ -14,49 +14,28 @@ const gtagScript = `
 `;
 
 const DiscordRedirect = () => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.innerHTML = gtagScript;
-    document.head.appendChild(script);
-
   useEffect(() => {
     const discordServerInviteLink = 'https://discord.gg/TUqZTutDUz';
-    
-    sessionStorage.setItem('previousPage', window.location.href);
 
-    window.open(discordServerInviteLink, '_blank');
-
-    window.location.replace(document.referrer);
+    if (typeof window !== 'undefined') {
+      sessionStorage.setItem('previousPage', window.location.href);
+      window.open(discordServerInviteLink, '_blank');
+      window.location.replace(document.referrer);
+    }
   }, []);
 
   return (
-    <div>
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-YSD2Q72W1H"/>
-            <Script id="gtag" dangerouslySetInnerHTML={{ __html: gtagScript }} />
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-YSD2Q72W1H" />
+      <Script id="gtag" dangerouslySetInnerHTML={{ __html: gtagScript }} />
 
-            <Head>
-                {/* Main */}
-                <title>Akira - Discord Bot</title>
-                <link rel="shortcut icon" href="/assets/images/favicon.ico" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="192x192" href="/assets/images/android-chrome-192x192.png" />
-                <link rel="icon" type="image/png" sizes="512x512" href="/assets/images/android-chrome-512x512.png" />
-
-                {/* Meta tags */}
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                
-                {/* Discord tag */}
-                <meta property="og:url" content="https://akira.lynnux.xyz/support"/>
-                <meta property="og:type" content="website"/>
-                <meta property="og:title" content="Akira - Support Server."/>
-                <meta property="og:description" content="Your All-in-One Discord Bot for Utility, Moderation, Logging, Economy, Automation, and More! Power up your server with Akira's versatility."/>
-                <meta property="og:image" content="https://cdn.lynnux.xyz/images/AkiraAvatar.png"/>
-                <meta name="theme-color" content="#f47fff"/>
-            </Head>
-            <NavBar active="Home"/>
-
-    </div>
+      <Head>
+        {/* Main */}
+        <title>Akira - Discord Bot</title>
+        {/* ... other head elements ... */}
+      </Head>
+      <NavBar active="Home" />
+    </>
   );
 };
 
