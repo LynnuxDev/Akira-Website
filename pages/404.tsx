47a5319ba79d728468d110d4dbd3d74ffd82d99b
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-13 23:03:20 +0100

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import Head from 'next/head';
 import Script from 'next/script';
 import Img from 'next/image';
+import Header from '../components/layout/header'
 
 import '../public/assets/css/style.css';
 import '../public/assets/css/main.css';
@@ -40,30 +41,7 @@ const Custom404 = () => {
             </Head>
 
             {/* Your HTML content goes here */}
-            <header>
-                <nav className=" navbar nav-bar " role="navigation " aria-label="main navigation ">
-                    <div className="navbar-brand ">
-                        <a className="navbar-item " href="./">
-                            <p className="nav-name ">Akira</p>
-                        </a>
-                        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbar-unfold">
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                        </a>
-                    </div>
-                    <div className="navbar-menu " id="navbar-unfold ">
-                        <div className="navbar-end ">
-                            <a className="navbar-item " href="../"><i className="nav-icons fas fa-home nav-fa"></i>Home</a>
-                            <a className="navbar-item " href="../commands "><i className="nav-icons fas fa-terminal nav-fa "></i>Commands</a>
-                            <a className="navbar-item " href="https://documentation.lynnux.xyz/ "><i className="nav-icons fas fa-book-open nav-fa "></i>Documentation</a>
-                            <a className="navbar-item " href="../permissions "><i className="nav-icons fas fa-check nav-fa "></i>Permissions</a>
-                            <a className="navbar-item " href="../support " target="_blank "><i className="nav-icons fab fa-discord nav-fa "></i>Support</a>
-                            <a className="navbar-item last-item " href="../invite " target="_blank "><i className="nav-icons fas fa-plus nav-fa "></i>Invite</a>
-                        </div>
-                    </div>
-                </nav>
-            </header>           
+            <Header/>
             <div className="Notfound_container___vm_Q">
                 <Img width="500" height="500"src="https://cdn.lynnux.xyz/assets/svg/NotFound.svg" alt="not-found" className="Notfound_not-found-img__st0se" />
 
