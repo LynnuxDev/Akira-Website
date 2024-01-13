e95d31cc28940fd41cd2ed64f4530054c8330181
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-13 23:23:14 +0100

    Update support.tsx

--- pages/support.tsx
+++ pages/support.tsx
@@ -32,7 +32,22 @@ const DiscordRedirect = () => {
       <Head>
         {/* Main */}
         <title>Akira - Discord Bot</title>
-        {/* ... other head elements ... */}
+        <link rel="shortcut icon" href="/assets/images/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/assets/images/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/assets/images/android-chrome-512x512.png" />
+
+        {/* Meta tags */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        
+        {/* Discord tag */}
+        <meta property="og:url" content="https://akira.lynnux.xyz/"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="Akira - Support Server."/>
+        <meta property="og:description" content="Your All-in-One Discord Bot for Utility, Moderation, Logging, Economy, Automation, and More! Power up your server with Akira's versatility."/>
+        <meta property="og:image" content="https://cdn.lynnux.xyz/images/AkiraAvatar.png"/>
+        <meta name="theme-color" content="#f47fff"/>
       </Head>
       <NavBar active="Home" />
     </>
