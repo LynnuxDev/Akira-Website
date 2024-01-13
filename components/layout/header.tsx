9b224792fc08ac90b935d0dadd1e6ad0e7d9e510
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-13 13:43:53 +0100

    Update header.tsx

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
 
-import '../../public/assets/css/navbar.css';
+import "../../public/assets/css/navbar.css"
 
 interface NavBarProps {
     active?: string;
@@ -14,12 +16,21 @@ interface NavBarProps {
     const isItemActive = (itemName: string) => {
       return active === itemName ? 'nav-active' : '';
     };
-  
+
     return (
-      <header>
+    <>    
+    <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+          integrity="sha512-..."
+          crossOrigin="anonymous"
+        />
+    </Head>
+    <header>
         <nav className="navbar nav-bar" role="navigation" aria-label="main navigation">
           <div className="navbar-brand">
-            <Link legacyBehavior href="/">
+            <Link legacyBehavior href="/" passHref>
                 <a className="navbar-item">
                     <p className="nav-name">Akira</p>
                 </a>
@@ -38,32 +49,32 @@ interface NavBarProps {
           </div>
           <div className="navbar-menu" id="navbar-unfold">
             <div className="navbar-end">
-                <Link legacyBehavior href="/">
+                <Link passHref legacyBehavior href="/">
                     <a className={`navbar-item ${isItemActive('Home')}`}>
                         <i className="nav-icons fas fa-home nav-fa"></i>Home
                     </a>
                 </Link>
-                <Link legacyBehavior href="/commands">
+                <Link passHref legacyBehavior href="/commands">
                     <a className={`navbar-item ${isItemActive('Commands')}`}>
                         <i className="nav-icons fas fa-terminal nav-fa"></i>Commands
                     </a>
                 </Link>
-                <Link legacyBehavior href="https://documentation.lynnux.xyz">
+                <Link passHref legacyBehavior href="https://documentation.lynnux.xyz">
                     <a className={`navbar-item ${isItemActive('Documentation')}`}>
                         <i className="nav-icons fas fa-book-open nav-fa"></i>Documentation
                     </a>
                 </Link>
-                <Link legacyBehavior href="/Permissions">
+                <Link passHref legacyBehavior href="/Permissions">
                     <a className={`navbar-item ${isItemActive('Permissions')}`}>
                         <i className="nav-icons fas fa-check nav-fa"></i>Permissions
                     </a>
                 </Link>
-                <Link legacyBehavior href="/Support">
+                <Link passHref legacyBehavior href="/Support">
                     <a className={`navbar-item ${isItemActive('Support')}`}>
                         <i className="nav-icons fab fa-discord nav-fa"></i>Support
                     </a>
                 </Link>
-                <Link legacyBehavior href="/Invite">
+                <Link passHref legacyBehavior href="/Invite">
                     <a className={`navbar-item last-item ${isItemActive('Invite')}`}>
                         <i className="nav-icons fas fa-plus nav-fa"></i>Invite
                     </a>
@@ -72,28 +83,8 @@ interface NavBarProps {
           </div>
         </nav>
       </header>
+      </>
     );
 };
   
 export default NavBar;
-  
-
-/*
-<header>
-<nav className=" navbar nav-bar " role="navigation " aria-label="main navigation ">
-    <div className="navbar-brand ">
-        <a className="navbar-item " href="./ ">
-            <p className="nav-name ">Akira</p>
-        </a><a role="button " className="navbar-burger burger " aria-label="menu " aria-expanded="false" data-target="navbar-unfold"><span aria-hidden="true"></span><span aria-hidden="true"></span><span aria-hidden="true"></span></a></div>
-    <div className="navbar-menu " id="navbar-unfold ">
-        <div className="navbar-end ">
-            <a className="navbar-item nav-active" href="/ "><i className="nav-icons fas fa-home nav-fa "></i>Home</a>
-            <a className="navbar-item " href="./commands "><i className="nav-icons fas fa-terminal nav-fa "></i>Commands</a>
-            <a className="navbar-item " href="https://documentation.lynnux.xyz/ "><i className="nav-icons fas fa-book-open nav-fa "></i>Documentation</a>
-            <a className="navbar-item " href="./permissions "><i className="nav-icons fas fa-check nav-fa "></i>Permissions</a>
-            <a className="navbar-item " href="./support " target="_blank "><i className="nav-icons fab fa-discord nav-fa "></i>Support</a>
-            <a className="navbar-item last-item " href="./invite " target="_blank "><i className="nav-icons fas fa-plus nav-fa "></i>Invite</a>
-        </div>
-    </div>
-</nav>
-</header> */
