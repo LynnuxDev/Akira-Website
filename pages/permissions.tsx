0216bdae554af86e2c622a2885293b03643cdb09
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2024-01-17 00:14:44 +0100

    Update permissions.tsx

--- pages/permissions.tsx
+++ pages/permissions.tsx
@@ -1,14 +1,58 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 import Footer from '../components/layout/footer';
 import NavBar from '../components/layout/header';
 
 import '../public/assets/css/style.css';
 
+const gtagScript = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-YSD2Q72W1H');
+`;
+
 const permissions: React.FC = () => {
+    useEffect(() => {
+    
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.innerHTML = gtagScript;
+        document.head.appendChild(script);
+    
+        return () => {
+          document.head.removeChild(script);
+        };
+      }, []);
 
     return (
         <>
+            <Script src="/assets/javascript/commands.js"/>
+            {/* Google tag */}
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-YSD2Q72W1H" />
+            <Script id="gtag" dangerouslySetInnerHTML={{ __html: gtagScript }} />
+
+            <Head>
+                {/* Main */}
+                <title>Akira - Permissions - Discord Bot</title>
+                <link rel="shortcut icon" href="/assets/images/favicon.ico" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="192x192" href="/assets/images/android-chrome-192x192.png" />
+                <link rel="icon" type="image/png" sizes="512x512" href="/assets/images/android-chrome-512x512.png" />
+
+                {/* Meta tags */}
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+                {/* Discord tag */}
+                <meta property="og:url" content="https://akira.lynnux.xyz/commands/"/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:title" content="Akira - Permissions."/>
+                <meta property="og:description" content="Looking for a comprehensive list of commands for Akira? You found it, you'll find all the information you need to use Akira to her fullest potential. From moderation to reaction-roles, and our Commands page makes it easy to find the features you're looking for. Akira's Commands page is an essential resource for getting the most out of our bot."/>
+                <meta property="og:image" content="https://cdn.lynnux.xyz/images/AkiraAvatar.png"/>
+                <meta name="theme-color" content="#f47fff"/>
+            </Head>
             <NavBar/>
             <div className="main">
                 <div className="MainPerms">
